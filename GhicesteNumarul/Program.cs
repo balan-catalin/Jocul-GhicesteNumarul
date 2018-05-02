020b70dc972366657b4ea0481eb1018c4aba7896
balan-catalin <foto.bcatalin@gmail.com>
2018-05-02 22:25:17 +0300

    Am creeat Jocul Ghiceste Numarul

--- GhicesteNumarul/Program.cs
+++ GhicesteNumarul/Program.cs
@@ -10,6 +10,21 @@ namespace GhicesteNumarul
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            int nr = rnd.Next(1, 128);
+            bool tf = false;
+            while (!tf)
+            {
+                Console.Write("Numarul tau este:");
+                int x = int.Parse(Console.ReadLine());
+                if (x < nr)
+                    Console.WriteLine("Numarul tau este mai MIC decat numarul pe care trebuie sa il ghicesti.");
+                else if (x > nr)
+                    Console.WriteLine("Numarul tau este mai MARE decat numarul pe care trebuie sa il ghicesti.");
+                else
+                    tf = true;
+            }
+            Console.WriteLine("Felicitari, ai ghicit numarul " + nr);
         }
     }
 }
